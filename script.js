c8e403bf40050f81bbe12a89c99c2e9b23b814d4
Taaaaab <owen.whitsett@gmail.com>
2022-08-30 17:26:20 -0400

    Fibonacci sequence function using iteration

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Fibonacci Sequence using Recursion
 
-function fibs(array) {
-    
+function fibs(n) {
+    let n1 = 0;
+    let n2 = 1;
+    let myArr = [n1, n2];
+
+while (myArr.length < n) {
+    let nextNum = n1 + n2;
+myArr.push(nextNum);
+n1 = n2;
+n2 = nextNum;
+
+}
+return myArr;
+}
+
+console.log(fibs(8));
+
+function fibsRec(n) {
+   
 }
